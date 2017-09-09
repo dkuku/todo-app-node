4236cb6ecbb74dec8722a7dec3232e11ce583141
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-09 15:27:03 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
     )
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({
+            todos
+        });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(3000, () => {
 console.log('server started on port 3000');
 });
