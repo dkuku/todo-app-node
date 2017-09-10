8e429cde0b2b9f6602144ad223a856ed9abef144
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-10 05:27:29 +0100

    test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,41 @@ describe('GET /todos/:id', () => {
             .end(done)
     });
 });
+
+describe('DELETE /todos/:id', () =>{
+    it('should remove  todo', (done) => {
+        var hexID = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexID);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexID).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done());
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var hexID = new ObjectID().toHexString();
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object id is not valid', (done) => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+
+});
