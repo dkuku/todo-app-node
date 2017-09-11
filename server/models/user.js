35760e1fc8417cc2e09bb2bf22353b9cc7316d62
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-11 14:30:14 +0100

    Basic POST /users route

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,34 @@
-var mongoose = require('mongoose');
-
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
-    name: {
+    password: {
         type: String,
         required: true,
-        minlenght: 1,
-        trim: true
-    }
+        minlenght: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        
+        }
+    }]
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -105,6 +105,21 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// Post /users
+//
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    var body = _.pick(req.body, ['email', 'password']);
+    console.log(body);
+    var user = new User(body);
+    console.log(user);
+    user.save().then((user) => {
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
 console.log(`server started on port ${port}`);
 });
