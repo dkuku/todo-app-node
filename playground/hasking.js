7dcd31b0bf28fce43086597cfb206bfc1a70b202
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-13 13:32:25 +0100

    hash user password before saving

--- playground/hasking.js
+++ playground/hasking.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-id: 10
-}
+var password = '123abc';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+    });
+});
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var hashedPassword = '$2a$10$7xpeqO1g8EiOivjUdeHndO0aoCWXK9XqhxWeyRfB6UpwTL7XmAugC';
 
-
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+//
+//var data = {
+//id: 10
+//}
+//
+//var token = jwt.sign(data, '123abc');
+//console.log(token);
+//
+//
+//var decoded = jwt.verify(token, '123abc');
+//console.log('decoded', decoded);
 //var message = 'I am user number 3'
 //var hash = SHA256(message).toString();
 //
