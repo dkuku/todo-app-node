121ed82c3c4774ab31ebca4a5728c0b1ba336932
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-15 14:50:52 +0100

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -223,6 +223,8 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
+                }).catch((e) => {
+                    done(e);
                 });
             });
     });
@@ -298,6 +300,53 @@ describe('POST /users/login',() => {
             })
             .end(done);
     });
+});
 
+describe('POST /users/login',() => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email, 
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.email).toBe(users[1].email);
+                expect(res.body._id).toExist();
+                expect(res.header['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err){
+                    return done(e);
+                }
+                
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                done();
+                }).catch((e) => {
+                    done(e);
+                });
 
+            });
+    });
+
+    it('should respond 400 for user wirh bad pass', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[0].email,
+                password: 'badPass'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.body).toEqual({});
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end(done);
+    });
+    
 });
