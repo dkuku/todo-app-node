3946973cb6595ec1bfb6aa5359262788ce0296f3
Daniel Kukula <daniel.kuku@gmail.com>
2017-09-15 15:05:42 +0100

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,18 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }
+    })
+};
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
